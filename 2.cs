933a638fb7448b0856d4feecf3b6b8cfc05d19eb
Анастасия Чевелева <nastasya@MacBook-Pro-Anastasia.local>
2022-12-17 22:09:46 +0300

    first commit

---
+++ 2.cs
@@ -0,0 +1,20 @@
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите число x: ");
+int x = Convert.ToInt32 (Console.ReadLine());
+int max = 0;
+if ( x < a & a > b)
+{
+    max = a;
+}
+else if (x < b & b > a)
+{
+    max = b;
+}
+else
+{
+    max = x;
+}
+Console.WriteLine($"{max}");
---
+++ 3.cs
@@ -0,0 +1,21 @@
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+   Console.WriteLine("нет");
+}
+
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+   Console.WriteLine("нет");
+}
