5b27f9898704f60599bae9d8d57db8baf004f1f3
kosmo-pirozhok <103745788+kosmo-pirozhok@users.noreply.github.com>
2023-01-22 14:13:04 +0300

    Задача 27
    Задача 27: Напишите программу, которая принимает 
    на вход число и выдаёт сумму цифр в числе.

---
+++ 27.cs
@@ -0,0 +1,13 @@
+int Division(int number)
+{
+    if (number < 10)
+        return number;
+    int end = number % 10;
+    int nextNumber = number / 10;
+    return end + Division(nextNumber);
+}
+
+int myNumber = new Random().Next(1, 300);
+Console.WriteLine($"Число = {myNumber}");
+int result = Division(myNumber);
+Console.WriteLine($"Сумма чисел = {result}");
