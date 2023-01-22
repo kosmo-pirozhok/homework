bc28e8ce839aeead1100ec378d0f5510c81e4506
kosmo-pirozhok <103745788+kosmo-pirozhok@users.noreply.github.com>
2023-01-22 14:24:09 +0300

    Задача 34
    Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
    Напишите программу, которая покажет количество чётных чисел в массиве.

---
+++ 34.cs
@@ -0,0 +1,32 @@
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i=0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] Array = CreateRandomArray(8, 100, 1000);
+int sum = 0;
+{
+    ShowArray(Array);
+        for (int i = 0; i < Array.Length; i++)
+        {
+            if (Array[i] % 2 == 0)
+            {
+                sum = sum + Array[i];
+            }
+        }
+        Console.WriteLine(sum);
+}
